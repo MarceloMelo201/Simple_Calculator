2f4de75a08d67c68f998ee5444a7b04ea0af184c
Marcelo Melo <marcelovmelo201@gmail.com>
2024-11-02 18:53:14 -0300

    commit

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,49 @@
+package application;
+
+import entities.Calculator;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        int response = 0, escolha;
+        boolean operationSuccess = true;
+        
+        System.out.print("Digite o primeiro número: ");
+        int numOne = sc.nextInt();
+
+        System.out.print("Digite o segundo número: ");
+        int numTwo = sc.nextInt();
+
+        System.out.println("Escolha a operação:\n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão");
+        escolha = sc.nextInt();
+
+        switch (escolha){
+            case 1:
+                response = Calculator.sum(numOne, numTwo);
+                break;
+                
+            case 2:
+                response = Calculator.subtraction(numOne, numTwo);
+                break;
+                
+            case 3:
+                response = Calculator.multiplication(numOne, numTwo);
+                break;
+                
+            case 4:
+                response = Calculator.division(numOne, numTwo);
+                break;
+                
+            default:
+                operationSuccess = false;
+                break; 
+        }
+
+        System.out.println((operationSuccess)? "A resposta é: " + response: "Opcção inválida.");
+
+
+        sc.close();
     }
 }
--- src/entities/Calculator.java
+++ src/entities/Calculator.java
@@ -1,2 +1,23 @@
-package entities;public class Calculator {
+package entities;
+
+public class Calculator {
+
+    public static int sum(int numOne, int numTwo){
+        return numOne + numTwo;
+    }
+
+    public static int subtraction(int numOne, int numTwo){
+        return numOne - numTwo;
+    }
+
+    public static int multiplication(int numOne, int numTwo){
+        return numOne * numTwo;
+    }
+
+    public static int division(int numOne, int numTwo){
+        if(numTwo == 0){
+            throw new IllegalArgumentException("Divisão por zero não é permitida.");
+        }
+        return numOne / numTwo;
+    }
 }
